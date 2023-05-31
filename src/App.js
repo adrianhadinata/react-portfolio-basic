c3f4a4c6c3b190b3bb2740a32a885b7cf628a13f
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-05-31 19:13:07 +0700

    Update: Final

--- src/App.js
+++ src/App.js
@@ -3,23 +3,63 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <Navbar nickname="Adrian"/>
+      <div className="Hero">
+        <LeftHero nickname="Adrian"/>
+        <RightHero/>
+      </div>
     </div>
   );
+}
+
+function Navbar (props) {
+  const { nickname } = props;
+  const title = <strong>{nickname}'s Portfolio</strong>
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+function LeftHero (props){
+  const myName = <strong>Adrian Hadinata Hadi Darsono</strong>
+  const myJob = <p>Full Stack Developer</p>
+  const { nickname } = props;
+
+  const projects = ["PT. Globalindo - Sewing Data Analytics (Full stack)", "PT. Globalindo - E-BUDGETING (Full stack)", "PT. Rehobat Sringin - Payroll Management System (Full stack)"];
+  const listProjects = projects.map((project) => 
+    <li>{ project }</li>
+  )
+
+  return (
+    <div className="MyText">
+      <h1>{myName}</h1>
+      {myJob}
+      <p>PHP | SQL | No SQL | CSS | Javascript | Python | Dart</p>
+      <p>CodeIgniter 3 & 4 | Laravel 8 | Bootstrap | Tailwind | React | Vue | JQuery | Node.js | Next.js | Flutter</p>
+      <ul>
+        { listProjects }
+      </ul>
+      <a onClick={(e) => ShowModal(nickname, e)} href="#">Let's connect with me ...</a>
+    </div>
+  )
+  
+}
+
+function RightHero (){
+  return (
+    <div className="MyPic">
+      <img src="adrianCasual.jpg" alt=""/>
+    </div>
+  )
+}
+
+function ShowModal (e) {
+  return alert('WA: 0858-0252-0642' + ' ('+ e +')')
+  // return console.log(e);
+  
 }
 
 export default App;
